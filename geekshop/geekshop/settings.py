34c281b99e8e66a9e2d39f3db552129de9e94291
[Aydyn79] <[mehtievaa@gmail.com]>
2022-01-27 21:02:49 +0700

    Lesson_5

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -29,7 +29,7 @@ load_dotenv(BASE_DIR/'.env')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
@@ -87,13 +87,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'geekshop.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres',
     }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
