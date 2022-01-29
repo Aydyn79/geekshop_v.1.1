806820be32a0640d1c6e289b838697f16dbcac4b
[Aydyn79] <[mehtievaa@gmail.com]>
2022-01-30 00:52:25 +0700

    Chapter_2_lesson_5

--- geekshop/static/js/order_scripts.js
+++ geekshop/static/js/order_scripts.js
@@ -86,6 +86,31 @@ window.onload = function () {
 
             });
 
+    $('.basket_list').on('click', 'input[type="number"]', function () {
+        let t_href = event.target
+        $.ajax(
+            {
+                url: "/basket/edit/" + t_href.name + "/" + t_href.value + "/",
+                success: function (data) {
+                    $('.basket_list').html(data.result)
+                },
+            });
+        event.preventDefault()
+    })
+
+    $('.card_add_basket').on('click', 'button[type="button"]', function () {
+        let t_href = event.target.value
+        $.ajax(
+            {
+                url: "/basket/add/" + t_href + "/",
+                success: function (data) {
+                    $('.card_add_basket').html(data.result)
+                    alert('товар добавлен вы корзину')
+                },
+            });
+        event.preventDefault()
+    //
+    })
 
 //    $('.order_form').on('click', 'input[type="checkbox"]', function () {
 //        var target = event.target;
